4bed94307c84b7a54607e1bfc6e9ba2e0a9332cc
TamarKhutchua <126088392+TamarKhutchua@users.noreply.github.com>
2023-08-31 16:36:17 +0400

    assign values

--- app.js
+++ app.js
@@ -30,3 +30,24 @@
   let dec = --sum;
   console.log("Decrement: " + dec);
 })();
+
+(function () {
+  let msg = "JavaScript";
+  msg += " Fun";
+  console.log("Add & concatenate: " + msg);
+  let sum = 5.0;
+  sum += 2.5;
+  console.log("Add & assign decimal: " + sum);
+  sum = 8;
+  sum -= 4;
+  console.log("Subtract & assign integer: " + sum);
+  sum = 8;
+  sum *= 4;
+  console.log("Multiply & assign integer: " + sum);
+  sum = 8;
+  sum /= 4;
+  console.log("Divide & assign integer: " + sum);
+  sum = 8;
+  sum %= 4;
+  console.log("Modulus & assign integer: " + sum);
+})();
