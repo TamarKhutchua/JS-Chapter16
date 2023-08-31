7bc8eb876bc2dd1b36a2ba0a81629287b73ea2e4
TamarKhutchua <126088392+TamarKhutchua@users.noreply.github.com>
2023-08-31 17:02:38 +0400

    assess logic

--- app.js
+++ app.js
@@ -69,3 +69,20 @@
   comparison = sum <= 8;
   console.log("Less Than or Equal To? " + comparison);
 })();
+
+(function () {
+  let result,
+    yes = true,
+    no = false;
+  result = yes && yes;
+  console.log("Are both true? " + result);
+  result = yes && no;
+  console.log("Are both still true?" + result);
+  result = yes || no;
+  console.log("Are either true? " + result);
+  result = no || no;
+  console.log("Are either still true? " + result);
+  console.log("Original value: " + yes);
+  yes = !yes;
+  console.log("Toggled value: " + yes);
+})();
