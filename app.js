3e89d38e02415efbd5326606f15c5f2d459087ca
TamarKhutchua <126088392+TamarKhutchua@users.noreply.github.com>
2023-08-31 16:45:13 +0400

    make comparisons

--- app.js
+++ app.js
@@ -51,3 +51,21 @@
   sum %= 4;
   console.log("Modulus & assign integer: " + sum);
 })();
+
+(function () {
+  let comparison,
+    sum = 8,
+    str = "JavaScript";
+  comparison = str === "JAVASCRIPT";
+  console.log("String Equality?" + comparison);
+  comparison = str === "JavaScript";
+  console.log("String Equality?" + comparison);
+  comparison = sum === 8;
+  console.log("Number Equality? " + comparison);
+  comparison = sum > 5;
+  console.log("Greater Than? " + comparison);
+  comparison = sum < 5;
+  console.log("Less Than? " + comparison);
+  comparison = sum <= 8;
+  console.log("Less Than or Equal To? " + comparison);
+})();
